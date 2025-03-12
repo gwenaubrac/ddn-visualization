7224b856660d2cc25c98b62b1ffb8740039122d2
Gwen Aubrac <Gwen.Aubrac@ladydavis.ca>
2025-03-12 10:55:01 -0400

    add test region

--- app/global.R
+++ app/global.R
@@ -31,99 +31,99 @@ hr_year <- read_excel(paste(path_data, 'hr_year.xlsx', sep = '/'))
 ## test start ##
 # artificially adding a region to test app
 # delete later
-x_by_month_bc <- x_by_month %>% 
+x_by_month_test <- x_by_month %>% 
   mutate_if(is.numeric, ~ . * 2) %>% 
   mutate(trt = if_else(trt == 2, 1, 0)) %>% 
-  mutate(region = "bc")
+  mutate(region = "test")
 
-x_by_month <- rbind(x_by_month, x_by_month_bc)
+x_by_month <- rbind(x_by_month, x_by_month_test)
 x_by_month %<>%
-  mutate(num_patients = if_else(region == "bc" & year_month == "2019-03-01", NA, num_patients))
+  mutate(num_patients = if_else(region == "test" & year_month == "2019-03-01", NA, num_patients))
 
-covs_bc <- covs %>% 
+covs_test <- covs %>% 
   mutate_if(is.numeric, ~ . * 2) %>% 
-  mutate(region = "bc") 
+  mutate(region = "test") 
 
-covs <- rbind(covs, covs_bc)
+covs <- rbind(covs, covs_test)
 
 covs %<>%
-  mutate(prop = if_else(region == "bc" & cov_name == "acute_renal", NA, prop)) %>% 
-  mutate(prop = if_else(region == "bc" & cov_name == "arrhythmia", NA, prop))
+  mutate(prop = if_else(region == "test" & cov_name == "acute_renal", NA, prop)) %>% 
+  mutate(prop = if_else(region == "test" & cov_name == "arrhythmia", NA, prop))
 
 
-ps_coef_bc <- ps_coef %>% 
+ps_coef_test <- ps_coef %>% 
   mutate_if(is.numeric, ~ . * 2) %>% 
-  mutate(region = "bc")
+  mutate(region = "test")
 
-ps_coef <- rbind(ps_coef, ps_coef_bc)
+ps_coef <- rbind(ps_coef, ps_coef_test)
 
 ps_coef$cov_name <- sub("1$", "", ps_coef$cov_name) # clean var name
 ps_coef %<>%
-  mutate(odds_ratio = if_else(region == "bc" & cov_name == "acute_renal", NA, odds_ratio)) %>% 
-  mutate(odds_ratio = if_else(region == "bc" & cov_name == "arrhythmia", NA, odds_ratio))
+  mutate(odds_ratio = if_else(region == "test" & cov_name == "acute_renal", NA, odds_ratio)) %>% 
+  mutate(odds_ratio = if_else(region == "test" & cov_name == "arrhythmia", NA, odds_ratio))
 
-smd_bc <- smd %>% 
+smd_test <- smd %>% 
   mutate_if(is.numeric, ~ . * 2) %>% 
-  mutate(region = "bc")
+  mutate(region = "test")
 
-smd <- rbind(smd, smd_bc)
+smd <- rbind(smd, smd_test)
 
 smd %<>%
-  mutate(SMD = if_else(region == "bc" & cov_name == "acute_renal", NA, SMD)) %>% 
-  mutate(SMD = if_else(region == "bc" & cov_name == "arrhythmia", NA, SMD))
+  mutate(SMD = if_else(region == "test" & cov_name == "acute_renal", NA, SMD)) %>% 
+  mutate(SMD = if_else(region == "test" & cov_name == "arrhythmia", NA, SMD))
 
-y_by_month_bc <- y_by_month %>% 
+y_by_month_test <- y_by_month %>% 
   mutate_if(is.numeric, ~ . * 2) %>% 
-  mutate(region = "bc") %>% 
+  mutate(region = "test") %>% 
   mutate(IRper100 = if_else(year_month == "2019-03-01", NA, IRper100))
 
-y_by_month <- rbind(y_by_month, y_by_month_bc)
+y_by_month <- rbind(y_by_month, y_by_month_test)
 
-hr_main_bc <- hr_main %>% 
+hr_main_test <- hr_main %>% 
   mutate_if(is.numeric, ~ . * 2) %>% 
-  mutate(region = "bc")
+  mutate(region = "test")
 
-hr_main <- rbind(hr_main, hr_main_bc) %>% 
-  mutate(hr_estimate = if_else(region == "bc" & comparison == "snri_vs_ssri", NA, hr_estimate)) %>% 
-  mutate(hr_ci_lower = if_else(region == "bc" & comparison == "snri_vs_ssri", NA, hr_ci_lower)) %>% 
-  mutate(hr_ci_upper = if_else(region == "bc" & comparison == "snri_vs_ssri", NA, hr_ci_upper))
+hr_main <- rbind(hr_main, hr_main_test) %>% 
+  mutate(hr_estimate = if_else(region == "test" & comparison == "snri_vs_ssri", NA, hr_estimate)) %>% 
+  mutate(hr_ci_lower = if_else(region == "test" & comparison == "snri_vs_ssri", NA, hr_ci_lower)) %>% 
+  mutate(hr_ci_upper = if_else(region == "test" & comparison == "snri_vs_ssri", NA, hr_ci_upper))
 
-hr_sens_bc <- hr_sens %>% 
+hr_sens_test <- hr_sens %>% 
   mutate_if(is.numeric, ~ . * 2) %>% 
-  mutate(region = "bc")
+  mutate(region = "test")
 
-hr_sens <- rbind(hr_sens, hr_sens_bc)
+hr_sens <- rbind(hr_sens, hr_sens_test)
 
-marg_bias_bc <- marg_bias %>% 
+marg_bias_test <- marg_bias %>% 
   mutate_if(is.numeric, ~ . * 2) %>% 
-  mutate(region = "bc")
+  mutate(region = "test")
 
-marg_bias <- rbind(marg_bias, marg_bias_bc)
+marg_bias <- rbind(marg_bias, marg_bias_test)
 
 marg_bias %<>%
-  mutate(bias = if_else(region == "bc" & cov_name == "acute_renal", NA, bias)) %>% 
-  mutate(bias = if_else(region == "bc" & cov_name == "arrhythmia", NA, bias))
+  mutate(bias = if_else(region == "test" & cov_name == "acute_renal", NA, bias)) %>% 
+  mutate(bias = if_else(region == "test" & cov_name == "arrhythmia", NA, bias))
 
-hr_age_bc <- hr_age %>%
+hr_age_test <- hr_age %>%
   mutate_if(is.numeric, ~ . * 2) %>%
-  mutate(region = "bc")
+  mutate(region = "test")
 
-hr_age <- rbind(hr_age, hr_age_bc) %>% 
-  mutate(old_hr_estimate = if_else(region == "bc" & comparison == "snri_vs_ssri", NA, old_hr_estimate)) %>% 
-  mutate(old_hr_ci_lower = if_else(region == "bc" & comparison == "snri_vs_ssri", NA, old_hr_ci_lower)) %>% 
-  mutate(old_hr_ci_upper = if_else(region == "bc" & comparison == "snri_vs_ssri", NA, old_hr_ci_upper))
+hr_age <- rbind(hr_age, hr_age_test) %>% 
+  mutate(old_hr_estimate = if_else(region == "test" & comparison == "snri_vs_ssri", NA, old_hr_estimate)) %>% 
+  mutate(old_hr_ci_lower = if_else(region == "test" & comparison == "snri_vs_ssri", NA, old_hr_ci_lower)) %>% 
+  mutate(old_hr_ci_upper = if_else(region == "test" & comparison == "snri_vs_ssri", NA, old_hr_ci_upper))
 
-hr_year_bc <- hr_year %>%
+hr_year_test <- hr_year %>%
   mutate_if(is.numeric, ~ . * 2) %>%
-  mutate(region = "bc")
+  mutate(region = "test")
 
-hr_year <- rbind(hr_year, hr_year_bc)
+hr_year <- rbind(hr_year, hr_year_test)
 
-hr_sex_bc <- hr_sex %>%
+hr_sex_test <- hr_sex %>%
   mutate_if(is.numeric, ~ . * 2) %>%
-  mutate(region = "bc")
+  mutate(region = "test")
 
-hr_sex <- rbind(hr_sex, hr_sex_bc)
+hr_sex <- rbind(hr_sex, hr_sex_test)
 
 ## test end ##
 
@@ -149,7 +149,7 @@ df_list <- list(x_by_month = x_by_month,
 df_list <- lapply(df_list, function(df) {
   df$region <- recode(df$region,
                       "cprd" = "United Kingdom",
-                      "bc" = "British Columbia")
+                      "test" = "Test")
   return(df)
 })
 
